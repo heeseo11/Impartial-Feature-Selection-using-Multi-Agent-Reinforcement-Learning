ec65e84630c65684c591b516ad39b3757e6544e1
seohee <61724682+heeseo11@users.noreply.github.com>
2024-10-04 14:18:23 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -5,27 +5,27 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 
-# 데이터 로드 및 전처리
+# Data Loading and Preprocessing
 data_path = './glucose data'
 EMR_path = './EMR.xlsx'
 all_data = load_and_preprocess_data(data_path, EMR_path)
 
-# Train/Test 데이터 분리
+# Train/Test Data Split
 train_data, test_data = split_train_test(all_data)
 
-# prepare_data 함수에서 train_EMR, test_EMR, train_X, test_X, y_train_class, y_test_class 반환
+# Return train_EMR, test_EMR, train_X, test_X, y_train_class, and y_test_class in the prepare_data function
 train_X, train_y, train_EMR = prepare_data(train_data)
 test_X, test_y, test_EMR = prepare_data(test_data)
 
-# 클래스 레이블 생성
+# Creating Class Labels
 train_class = create_class_labels(train_y)
 test_class = create_class_labels(test_y)
 
-# 리스트를 NumPy 배열로 변환한 후 astype 메서드 적용
+# Convert List to NumPy Array and Apply astype Method
 train_class = np.array(train_class)
 test_class = np.array(test_class)
 
-# 원-핫 인코딩
+# One-Hot Encoding
 y_train_class = to_categorical(train_class.astype('float32'))
 y_test_class = to_categorical(test_class.astype('float32'))
 
@@ -48,7 +48,7 @@ def accuracy_func(features, train_EMR, test_EMR, train_X, test_X, y_train_class,
 
 
 
-# Q-learning 및 모델 학습
+# Q-Learning and Model Training
 Q_values = np.array([[-1, -1]] * 10)
 
 all_rewards, updated_Q_values = q_learning(
